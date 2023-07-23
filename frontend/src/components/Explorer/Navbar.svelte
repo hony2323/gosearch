2f3cc23a965417b689a0a6c2b0dadb8c8b6643b0
hony <hony2323@gmail.com>
2023-07-23 20:23:24 +0300

    You can go back now

---
+++ frontend/src/components/Explorer/Navbar.svelte
@@ -0,0 +1,22 @@
+<script>
+  // Only import what you need!
+  import { AirplayIcon } from "svelte-feather-icons";
+  import { path, removeEntryFromPath } from "./../store/store";
+  const handleBackClick = () => {
+    path.update((value) => removeEntryFromPath(value));
+  };
+</script>
+
+<div class="navbar">
+  <div on:dblclick={() => handleBackClick()}>
+    <AirplayIcon size="24" on:click={() => handleBackClick()} />
+  </div>
+  <h2>{$path}</h2>
+</div>
+
+<style lang="scss">
+  .navbar {
+    display: flex;
+    justify-content: start;
+  }
+</style>
--- frontend/src/components/store/store.ts
+++ frontend/src/components/store/store.ts
@@ -9,3 +9,18 @@ export const addEntryToPath = (newEntryName, oldPath: string) => {
     return oldPath + newEntryName + "/"
 };
 
+
+export const removeEntryFromPath = (oldPath: string) => {
+    let thePath = oldPath.slice(0, -1);
+    const lastSeparatorIndex = thePath.lastIndexOf('/');
+    const separatorCount = thePath.split("/").length - 1;
+    if (separatorCount <= 0) {
+        return oldPath;
+    }
+    if (lastSeparatorIndex !== -1) {
+        return oldPath.substring(0, lastSeparatorIndex) + "/";
+    } else {
+        // If there is no path separator, return the original path
+        return oldPath;
+    }
+}
